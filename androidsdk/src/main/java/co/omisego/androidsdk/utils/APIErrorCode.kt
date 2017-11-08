5dce49d01d29abd218e0fcc7cba2805a60712da0
euro <phuchit@omise.co>
2017-11-08 10:46:05 +0700

    Add error codes constant
    Summary: Just add all error codes that I copied from the iOS SDK
    
    Test Plan: Run all tests
    
    Reviewers: thibault, mederic
    
    Reviewed By: thibault, mederic
    
    Maniphest Tasks: T296
    
    Differential Revision: https://phabricator.omisego.io/D139

---
+++ androidsdk/src/main/java/co/omisego/androidsdk/utils/APIErrorCode.kt
@@ -0,0 +1,22 @@
+package co.omisego.androidsdk.utils
+
+
+/**
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 11/7/2017 AD.
+ * Copyright © 2017 OmiseGO. All rights reserved.
+ */
+
+// Represents all error codes.
+object APIErrorCode {
+    val CLIENT_INVALID_PARAMETER = "client:invalid_parameter"
+    val CLIENT_INVALID_VERSION = "client:invalid_version"
+    val CLIENT_PERMISSION_ERROR = "client:permission_error"
+    val CLIENT_ENDPOINT_NOT_FOUND = "client:endpoint_not_found"
+    val CLIENT_INVALID_API_KEY = "client:invalid_api_key"
+    val SERVER_INTERNAL_SERVER_ERROR = "server:internal_server_error"
+    val SERVER_UNKNOWN_ERROR = "server:unknown_error"
+    val SERVER_ACCESS_TOKEN_NOT_FOUND = "server:access_token_not_found"
+    val SERVER_ACCESS_TOKEN_EXPIRED = "server:access_token_expired"
+}
