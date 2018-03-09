4ec86c22f97756e436a9e6bd494604fe67283400
euro <phuchit@omise.co>
2018-03-09 23:42:56 +0700

    Move file header

--- omisego-sdk/src/main/java/co/omisego/omisego/custom/retrofit2/adapter/OMGCall.kt
+++ omisego-sdk/src/main/java/co/omisego/omisego/custom/retrofit2/adapter/OMGCall.kt
@@ -1,14 +1,14 @@
 package co.omisego.omisego.custom.retrofit2.adapter
 
-import co.omisego.omisego.custom.Callback
-import retrofit2.Response
-
 /**
  * OmiseGO
  *
  * Created by Phuchit Sirimongkolsathien on 8/3/2018 AD.
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
+
+import co.omisego.omisego.custom.Callback
+import retrofit2.Response
 
 interface OMGCall<T> {
     fun cancel()
