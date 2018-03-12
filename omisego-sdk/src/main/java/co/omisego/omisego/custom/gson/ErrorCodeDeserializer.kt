34797dca4c65c99f39e37aabe551251d4a9d563e
euro <phuchit@omise.co>
2018-03-12 18:45:50 +0700

    Move license to below the package

--- omisego-sdk/src/main/java/co/omisego/omisego/custom/gson/ErrorCodeDeserializer.kt
+++ omisego-sdk/src/main/java/co/omisego/omisego/custom/gson/ErrorCodeDeserializer.kt
@@ -1,18 +1,18 @@
 package co.omisego.omisego.custom.gson
 
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 12/3/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
 import co.omisego.omisego.constant.ErrorCode
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import java.lang.reflect.Type
 
-
-/**
- * OmiseGO
- *
- * Created by Phuchit Sirimongkolsathien on 12/3/2018 AD.
- * Copyright © 2017-2018 OmiseGO. All rights reserved.
- */
 class ErrorCodeDeserializer : JsonDeserializer<ErrorCode> {
     override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ErrorCode {
         return ErrorCode.from(json.asString)
