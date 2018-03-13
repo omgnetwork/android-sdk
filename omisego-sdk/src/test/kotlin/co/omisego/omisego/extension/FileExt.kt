b57aa50d974a8de23aea53d5f68b68e7384e8fee
euro <phuchit@omise.co>
2018-03-13 15:17:31 +0700

    Add ErrorCodeDeserializerTest

--- omisego-sdk/src/test/kotlin/co/omisego/omisego/extension/FileExt.kt
+++ omisego-sdk/src/test/kotlin/co/omisego/omisego/extension/FileExt.kt
@@ -1,16 +1,15 @@
 package co.omisego.omisego.extension
 
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import java.io.File
-
-
-/**
+/*
  * OmiseGO
  *
  * Created by Phuchit Sirimongkolsathien on 13/3/2018 AD.
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
+
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import java.io.File
 
 fun File.mockEnqueueWithHttpCode(mockWebServer: MockWebServer, responseCode: Int = 200) {
     mockWebServer.enqueue(MockResponse().apply {
