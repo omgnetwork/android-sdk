b985e90350996106d27e80e4be6b5fa078b70319
euro <phuchit@omise.co>
2018-03-22 16:41:17 +0700

    Change TransactionListParams to data class

--- omisego-sdk/src/main/java/co/omisego/omisego/model/transaction/TransactionListParams.kt
+++ omisego-sdk/src/main/java/co/omisego/omisego/model/transaction/TransactionListParams.kt
@@ -14,58 +14,58 @@ import co.omisego.omisego.model.pagination.SortDirection
  *  Represent a structure used to query a list of transactions
  */
 
-class TransactionListParams {
-    /**
-     * A page number
-     */
-    private var page: Int = 1
+data class TransactionListParams internal constructor(
+        /**
+         * A page number
+         */
+        var page: Int = 1,
 
-    /**
-     * A number of results per page.
-     */
-    private var perPage: Int = 10
+        /**
+         * A number of results per page.
+         */
+        var perPage: Int = 10,
 
-    /**
-     * The sorting field
-     *
-     * The available values are
-     * - [Paginable.Transaction.SortableFields.ID]
-     * - [Paginable.Transaction.SortableFields.STATUS]
-     * - [Paginable.Transaction.SortableFields.FROM]
-     * - [Paginable.Transaction.SortableFields.CREATED_AT]
-     * - [Paginable.Transaction.SortableFields.UPDATED_AT]
-     */
-    private var sortBy: Paginable.Transaction.SortableFields = Paginable.Transaction.SortableFields.CREATED_AT
+        /**
+         * The sorting field
+         *
+         * The available values are
+         * - [Paginable.Transaction.SortableFields.ID]
+         * - [Paginable.Transaction.SortableFields.STATUS]
+         * - [Paginable.Transaction.SortableFields.FROM]
+         * - [Paginable.Transaction.SortableFields.CREATED_AT]
+         * - [Paginable.Transaction.SortableFields.UPDATED_AT]
+         */
+        var sortBy: Paginable.Transaction.SortableFields = Paginable.Transaction.SortableFields.CREATED_AT,
 
-    /**
-     * The desired sort direction
-     *
-     * The available values are
-     * - [SortDirection.ASCENDING]
-     * - [SortDirection.DESCENDING]
-     */
-    private var sortDirection: SortDirection = SortDirection.DESCENDING
+        /**
+         * The desired sort direction
+         *
+         * The available values are
+         * - [SortDirection.ASCENDING]
+         * - [SortDirection.DESCENDING]
+         */
+        var sortDirection: SortDirection = SortDirection.DESCENDING,
 
-    /**
-     * A term to search for in all of the searchable fields.
-     * See more at [Paginable.Transaction.SearchableFields]
-     *
-     * Note: Conflict with searchTerms, only use one of them.
-     */
-    private var searchTerm: String? = null
+        /**
+         * A term to search for in all of the searchable fields.
+         * See more at [Paginable.Transaction.SearchableFields]
+         *
+         * Note: Conflict with searchTerms, only use one of them.
+         */
+        var searchTerm: String? = null,
 
-    /**
-     * A key-value map to search with the available fields
-     * See more at [Paginable.Transaction.SearchableFields]
-     *
-     */
-    private var searchTerms: Map<Paginable.Transaction.SearchableFields, Any>? = null
+        /**
+         * A key-value map to search with the available fields
+         * See more at [Paginable.Transaction.SearchableFields]
+         *
+         */
+        var searchTerms: Map<Paginable.Transaction.SearchableFields, Any>? = null,
 
-    /**
-     * An optional address that belongs to the current user (primary address by default)
-     */
-    private var address: String? = null
-
+        /**
+         * An optional address that belongs to the current user (primary address by default)
+         */
+        var address: String? = null
+) {
     companion object {
         fun create(
                 page: Int = 1,
