acca67f0d1707c37fa1f77e7c5f4db87c0023ba2
Phuchit Sirimongkolsathien <phuchit@omise.co>
2018-06-06 15:53:43 +0700

    Add Account and Avatar model (#28)

---
+++ omisego-sdk/src/main/java/co/omisego/omisego/model/Avatar.kt
@@ -0,0 +1,23 @@
+package co.omisego.omisego.model
+
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 25/5/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
+/**
+ * Represents an avatar containing urls of different sizes.
+ *
+ * @param original The url of the original image.
+ * @param large The url of the large image.
+ * @param small The url of the small image.
+ * @param thumbnail The url of the thumbnail image.
+ */
+data class Avatar(
+    val original: String,
+    val large: String,
+    val small: String,
+    val thumbnail: String
+)
