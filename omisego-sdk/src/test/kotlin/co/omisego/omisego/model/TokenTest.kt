4238adfcb1bf001e75ba230d65ab3d7b73cd1971
euro <phuchit@omise.co>
2018-06-29 17:44:01 +0700

    Add token parsing test

--- omisego-sdk/src/test/kotlin/co/omisego/omisego/model/TokenTest.kt
+++ omisego-sdk/src/test/kotlin/co/omisego/omisego/model/TokenTest.kt
@@ -9,14 +9,22 @@ package co.omisego.omisego.model
  */
 
 import co.omisego.omisego.extension.bd
+import co.omisego.omisego.helpers.delegation.ResourceFile
+import co.omisego.omisego.testUtils.DateConverter
+import co.omisego.omisego.utils.GsonProvider
+import org.amshove.kluent.shouldEqual
 import org.amshove.kluent.shouldEqualTo
 import org.junit.Test
+import java.util.Date
 
 class TokenTest {
-    val token1 = Token("OMG:8bcda572-9411-43c8-baae-cd56eb0155f3", "OMG", "OmiseGO", 10000.0.bd, mapOf(), mapOf())
-    val token2 = Token("ETH:8bcda572-9411-43c8-baae-cd56eb0155f3", "ETH", "Ethereum", 10000.0.bd, mapOf(), mapOf())
-    val token3 = Token("OMG:8bcda572-9411-43c8-baae-cd56eb0155f3", "OMG", "OmiseGO", 10.0.bd, mapOf(), mapOf())
-    val token4 = Token("OMG:8bcda572-9411-43c8-baae-cd56eb0155f3", "OMG", "OmiseGO", 10000.0.bd, mapOf(), mapOf())
+    private val dateConverter by lazy { DateConverter() }
+    val tokenFile by ResourceFile("token.json", "object")
+    val gson by lazy { GsonProvider.create() }
+    val token1 = Token("OMG:8bcda572-9411-43c8-baae-cd56eb0155f3", "OMG", "OmiseGO", 10000.0.bd, Date(), Date(), mapOf(), mapOf())
+    val token2 = Token("ETH:8bcda572-9411-43c8-baae-cd56eb0155f3", "ETH", "Ethereum", 10000.0.bd, Date(), Date(), mapOf(), mapOf())
+    val token3 = Token("OMG:8bcda572-9411-43c8-baae-cd56eb0155f3", "OMG", "OmiseGO", 10.0.bd, Date(), Date(), mapOf(), mapOf())
+    val token4 = Token("OMG:8bcda572-9411-43c8-baae-cd56eb0155f3", "OMG", "OmiseGO", 10000.0.bd, Date(), Date(), mapOf(), mapOf())
 
     @Test
     fun `token1 should be not compatible with token2`() {
@@ -31,5 +39,20 @@ class TokenTest {
     @Test
     fun `token1 should be compatible with token4`() {
         token1 compatWith token4 shouldEqualTo true
+    }
+
+    @Test
+    fun `token should be parsed correctly`() {
+        val token = gson.fromJson<Token>(tokenFile.readText(), Token::class.java)
+        with(token) {
+            id shouldEqual "tok_ETH_01cbfge9qhmsdbjyb7a8e8pxt3"
+            symbol shouldEqual "ETH"
+            name shouldEqual "Ethereum"
+            subunitToUnit shouldEqual 100000.bd
+            metadata shouldEqual kotlin.collections.mapOf<String, Any>()
+            encryptedMetadata shouldEqual kotlin.collections.mapOf<String, Any>()
+            createdAt shouldEqual dateConverter.fromString("2018-01-01T00:00:00Z")
+            updatedAt shouldEqual dateConverter.fromString("2018-01-01T00:00:00Z")
+        }
     }
 }
