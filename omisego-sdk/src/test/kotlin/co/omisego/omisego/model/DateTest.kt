e54f3bce20ad8f57aa840c339e85bd8eaab051a8
euro <phuchit@omise.co>
2018-07-02 13:25:03 +0700

    Fix date test failed

--- omisego-sdk/src/test/kotlin/co/omisego/omisego/model/DateTest.kt
+++ omisego-sdk/src/test/kotlin/co/omisego/omisego/model/DateTest.kt
@@ -7,10 +7,12 @@ package co.omisego.omisego.model
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
 
+import co.omisego.omisego.extension.toCalendar
 import co.omisego.omisego.helpers.delegation.GsonDelegator
 import co.omisego.omisego.helpers.delegation.ResourceFile
 import com.google.gson.reflect.TypeToken
-import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldEqualTo
+import org.amshove.kluent.shouldNotBe
 import org.junit.Test
 import java.util.Calendar
 import java.util.Date
@@ -24,19 +26,34 @@ class DateTest : GsonDelegator() {
 
     @Test
     fun `date should be parsed correctly`() {
-        multiFormatDate["date_1"]?.compareTo(Calendar.getInstance().apply {
-            set(2018, 0, 1, 8, 0, 0)
-            set(Calendar.MILLISECOND, 0)
-        }.time) shouldBe 0
+        multiFormatDate["date_1"] shouldNotBe null
+        with(multiFormatDate["date_1"]!!.toCalendar()) {
+            get(Calendar.DAY_OF_MONTH) shouldEqualTo 1
+            get(Calendar.MONTH) shouldEqualTo 0
+            get(Calendar.YEAR) shouldEqualTo 2018
+            get(Calendar.HOUR_OF_DAY) shouldEqualTo 8
+            get(Calendar.MINUTE) shouldEqualTo 0
+            get(Calendar.SECOND) shouldEqualTo 0
+        }
 
-        multiFormatDate["date_2"]?.compareTo(Calendar.getInstance().apply {
-            set(2018, 0, 1, 9, 0, 0)
-            set(Calendar.MILLISECOND, 0)
-        }.time) shouldBe 0
+        multiFormatDate["date_2"] shouldNotBe null
+        with(multiFormatDate["date_2"]!!.toCalendar()) {
+            get(Calendar.DAY_OF_MONTH) shouldEqualTo 1
+            get(Calendar.MONTH) shouldEqualTo 0
+            get(Calendar.YEAR) shouldEqualTo 2018
+            get(Calendar.HOUR_OF_DAY) shouldEqualTo 9
+            get(Calendar.MINUTE) shouldEqualTo 0
+            get(Calendar.SECOND) shouldEqualTo 0
+        }
 
-        multiFormatDate["date_3"]?.compareTo(Calendar.getInstance().apply {
-            set(2018, 0, 1, 10, 0, 0)
-            set(Calendar.MILLISECOND, 0)
-        }.time) shouldBe 0
+        multiFormatDate["date_3"] shouldNotBe null
+        with(multiFormatDate["date_3"]!!.toCalendar()) {
+            get(Calendar.DAY_OF_MONTH) shouldEqualTo 1
+            get(Calendar.MONTH) shouldEqualTo 0
+            get(Calendar.YEAR) shouldEqualTo 2018
+            get(Calendar.HOUR_OF_DAY) shouldEqualTo 10
+            get(Calendar.MINUTE) shouldEqualTo 0
+            get(Calendar.SECOND) shouldEqualTo 0
+        }
     }
 }
