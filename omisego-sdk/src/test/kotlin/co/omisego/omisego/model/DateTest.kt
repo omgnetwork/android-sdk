4c707b14b3369fb05f73ef3855aa77ec1537bf3b
euro <phuchit@omise.co>
2018-07-02 16:33:03 +0700

    Improve date test

--- omisego-sdk/src/test/kotlin/co/omisego/omisego/model/DateTest.kt
+++ omisego-sdk/src/test/kotlin/co/omisego/omisego/model/DateTest.kt
@@ -9,16 +9,16 @@ package co.omisego.omisego.model
 
 import co.omisego.omisego.helpers.delegation.GsonDelegator
 import co.omisego.omisego.helpers.delegation.ResourceFile
+import co.omisego.omisego.testUtils.DateConverter
 import com.google.gson.reflect.TypeToken
 import org.amshove.kluent.shouldEqual
 import org.amshove.kluent.shouldNotBe
 import org.junit.Test
-import java.text.ParseException
-import java.text.SimpleDateFormat
 import java.util.Date
 
 class DateTest : GsonDelegator() {
     private val dateFile by ResourceFile("date.json", "object")
+    private val dateFormatter: DateConverter by lazy { DateConverter() }
     private val multiFormatDate by lazy {
         val typeToken = object : TypeToken<Map<String, Date>>() {}.type
         gson.fromJson<Map<String, Date>>(dateFile.readText(), typeToken)
@@ -27,25 +27,10 @@ class DateTest : GsonDelegator() {
     @Test
     fun `date should be parsed correctly`() {
         multiFormatDate["date_1"] shouldNotBe null
-        multiFormatDate["date_1"] shouldEqual "2018-01-01T01:00:00.000000Z".toDate()
+        multiFormatDate["date_1"] shouldEqual dateFormatter.fromString("2018-01-01T01:00:00.000000Z")
         multiFormatDate["date_2"] shouldNotBe null
-        multiFormatDate["date_2"] shouldEqual "2018-01-01T02:00:00.000Z".toDate()
+        multiFormatDate["date_2"] shouldEqual dateFormatter.fromString("2018-01-01T02:00:00.000Z")
         multiFormatDate["date_3"] shouldNotBe null
-        multiFormatDate["date_3"] shouldEqual "2018-01-01T03:00:00Z".toDate()
-    }
-
-    private fun String.toDate(): Date? {
-        val formats: List<String> = listOf(
-            "yyyy-MM-dd'T'HH:mm:ssX",
-            "yyyy-MM-dd'T'HH:mm:ss.SSSX",
-            "yyyy-MM-dd'T'HH:mm:ss.SSSSSSX"
-        )
-        for (format in formats) {
-            try {
-                return SimpleDateFormat(format).parse(this)
-            } catch (e: ParseException) {
-            }
-        }
-        return null
+        multiFormatDate["date_3"] shouldEqual dateFormatter.fromString("2018-01-01T03:00:00Z")
     }
 }
