0282f1be7da7e85153b4fcc85e2a3bf762153216
euro <phuchit@omise.co>
2018-07-03 17:07:16 +0700

    Add liveTest for get_transactions API

---
+++ omisego-sdk/src/liveTests/kotlin/co/omisego/omisego/TransactionLiveTest.kt
@@ -0,0 +1,31 @@
+package co.omisego.omisego
+
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 3/7/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
+import co.omisego.omisego.model.transaction.list.TransactionListParams
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeInstanceOf
+import org.amshove.kluent.shouldNotBe
+import org.junit.Test
+
+class TransactionLiveTest : LiveTest() {
+
+    @Test
+    fun `get_transactions should return 200 and parsed the response correctly`() {
+        val transactions = client.getTransactions(TransactionListParams.create(searchTerm = null)).execute()
+        transactions.isSuccessful shouldBe true
+        transactions.body()?.data shouldNotBe null
+        with(transactions.body()?.data!!) {
+            this.pagination shouldBeInstanceOf co.omisego.omisego.model.pagination.Pagination::class.java
+            this.data shouldBeInstanceOf kotlin.collections.List::class.java
+            if (this.data.isNotEmpty()) {
+                this.data[0] shouldBeInstanceOf co.omisego.omisego.model.transaction.Transaction::class.java
+            }
+        }
+    }
+}
