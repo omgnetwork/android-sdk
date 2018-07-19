0305df3dc0efa612316708ef9089b7574d324e99
euro <phuchit@omise.co>
2018-07-19 17:05:20 +0700

    Add FilterStrategy

---
+++ omisego-sdk/src/main/java/co/omisego/omisego/websocket/strategy/FilterStrategy.kt
@@ -0,0 +1,27 @@
+package co.omisego.omisego.websocket.strategy
+
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 19/7/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
+import co.omisego.omisego.model.socket.SocketTopic
+import co.omisego.omisego.websocket.event.SocketEvent
+
+sealed class FilterStrategy {
+    abstract fun accept(event: SocketEvent<*>): Boolean
+
+    class Event(private val allowedEvents: List<Class<out SocketEvent<*>>>) : FilterStrategy() {
+        override fun accept(event: SocketEvent<*>) = allowedEvents.any { it.isAssignableFrom(event::class.java) }
+    }
+
+    class Topic(val topic: SocketTopic) : FilterStrategy() {
+        override fun accept(event: SocketEvent<*>) = topic.name == event.socketReceive.topic
+    }
+
+    class None : FilterStrategy() {
+        override fun accept(event: SocketEvent<*>) = true
+    }
+}
