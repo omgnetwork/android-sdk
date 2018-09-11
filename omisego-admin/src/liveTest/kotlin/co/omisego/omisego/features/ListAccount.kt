48ec25766b22a48975f687306b0f75a24f43d3c2
euro <phuchit@omise.co>
2018-09-12 00:13:51 +0700

    :white_check_mark: Add list account test

---
+++ omisego-admin/src/liveTest/kotlin/co/omisego/omisego/features/ListAccount.kt
@@ -0,0 +1,50 @@
+package co.omisego.omisego.features
+
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 11/9/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
+import co.omisego.omisego.LiveTest
+import co.omisego.omisego.model.params.AccountListParams
+import co.omisego.omisego.model.params.LoginParams
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldBeGreaterThan
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [23])
+class ListAccount : LiveTest() {
+    private val secret by lazy { loadSecretFile("secret.json") }
+
+    @Before
+    fun setup() {
+        val response = client.login(
+            LoginParams(
+                secret.getString("email"),
+                secret.getString("password")
+            )
+        ).execute()
+        response.isSuccessful shouldBe true
+    }
+
+    @Test
+    fun `list account should be returned successfully`() {
+        val response = client.getAccounts(
+            AccountListParams.create(
+                searchTerm = null
+            )
+        ).execute()
+
+        response.isSuccessful shouldBe true
+        response.body()?.data?.pagination?.perPage shouldBe 10
+        response.body()?.data?.pagination?.currentPage shouldBe 1
+        response.body()?.data?.data?.size!! shouldBeGreaterThan 0
+    }
+}
