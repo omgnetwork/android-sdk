7929e4484dd1030bf8c55e34c3f7861b1777b3c1
euro <phuchit@omise.co>
2018-09-11 15:35:46 +0700

    :tada: Init live test

---
+++ omisego-admin/src/liveTest/kotlin/co/omisego/omisego/LiveTest.kt
@@ -0,0 +1,30 @@
+package co.omisego.omisego
+
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 3/7/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
+import co.omisego.omisego.OMGAPIAdmin
+import co.omisego.omisego.model.AdminConfiguration
+import co.omisego.omisego.network.ewallet.EWalletAdmin
+import co.omisego.omisego.utils.ResourceFileLoader
+
+open class LiveTest : ResourceFileLoader() {
+    private val secret by lazy { loadSecretFile("secret.json") }
+    private val config by lazy {
+        AdminConfiguration(
+            secret.getString("base_url")
+        )
+    }
+    private val eWalletAdmin by lazy {
+        EWalletAdmin.Builder {
+            clientConfiguration = config
+        }.build()
+    }
+    val client by lazy {
+        OMGAPIAdmin(eWalletAdmin)
+    }
+}
