19bf86135620245a8e37df920613e73dfb88f600
euro <phuchit@omise.co>
2018-11-23 23:08:27 +0700

    :sparkles: Add filterable fields for `transaction.all`

---
+++ omisego-core/src/main/java/co/omisego/omisego/model/filterable/Filterable.kt
@@ -0,0 +1,27 @@
+package co.omisego.omisego.model.filterable
+
+/*
+ * OmiseGO
+ *
+ * Created by Phuchit Sirimongkolsathien on 23/11/2018 AD.
+ * Copyright © 2017-2018 OmiseGO. All rights reserved.
+ */
+
+sealed class Filterable {
+    /**
+     * Represents transaction's filterable fields
+     */
+    class TransactionFields : Filterable() {
+        val id = "id"
+        val status = "status"
+        val errorCode = "error_code"
+        val errorDescription = "error_description"
+        val type = "type"
+        val fromAmount = "from_amount"
+        val toAmount = "to_amount"
+        val calculatedAt = "calculated_at"
+        val insertedAt = "inserted_at"
+        val createdAt = "created_at"
+        val updatedAt = "updated_at"
+    }
+}
