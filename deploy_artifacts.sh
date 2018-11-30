5d62c3b85aa4fefbc8142b987bfa2fec1c9e0518
euro <phuchit@omise.co>
2018-11-30 12:02:48 +0700

    :zap: Improve deployment build time

--- deploy_artifacts.sh
+++ deploy_artifacts.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 modules=("omisego-core" "omisego-client" "omisego-admin")
 
+#./gradlew clean assemble # Without tests
+./gradlew clean build # With tests
+
 for module in "${modules[@]}"
 do
   echo ">> Deploying $module ..."
-  ./gradlew :$module:clean :$module:build :$module:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
+    ./gradlew $module:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
   echo ">> Done deploying for $module"
 done
